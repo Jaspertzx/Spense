f89a7e9d298a5b4924c47e37a4234140b6d6141d
Jaspertzx <jaspertzx905@gmail.com>
2023-05-16 15:36:54 +0800

    Init files
    Template files for Sprint Boot 3.+ Java 17

---
+++ src/main/java/com/spense_be/spense/SpenseController.java
@@ -0,0 +1,13 @@
+package com.spense_be.spense;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpenseController {
+    
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Qin Yong!";
+    }
+}
