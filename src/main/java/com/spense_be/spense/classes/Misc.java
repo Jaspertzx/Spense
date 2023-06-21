55236f6dd3955d3ab600c878d4f6ac96ff8bda13
tanqinyong <tqinyong@yahoo.com.sg>
2023-06-21 22:52:05 +0800

    [V1.1.31] Azure Test 3
    Create new build for Azure

--- src/main/java/com/spense_be/spense/classes/Misc.java
+++ src/main/java/com/spense_be/spense/classes/Misc.java
@@ -3,5 +3,5 @@ package com.spense_be.spense.classes;
 //extremely ambiguous class... for socialmedia, anything extra.... idk if this is a good idea
 public class Misc {
 
-    // AZURE TEST 2
+    // AZURE TEST 3
 }
